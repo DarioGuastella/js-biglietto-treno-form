1ec379cbba66036e25b6c81e7cf0aaaaab1cddff
DarioGuastella <jimmyguastella@gmail.com>
2023-10-27 16:49:10 +0200

    aggiunge stile

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
- let nameInput = document.querySelector("#username");
- let kmInput = document.querySelector("#kilometers");
- let ageInput = document.querySelector("#ageselect");
- let btn = document.querySelector("#generateBtn");
+const kmtariff = 0.21
+let nameInput = document.querySelector("#username");
+let kmInput = document.querySelector("#kilometers");
+let ageInput = document.querySelector("#ageselect");
+let btn = document.querySelector("#generateBtn");
  
 
- btn.addEventListener("click", function() {
-     console.log("L'utente ha scritto: " + nameInput.value);
-     console.log("L'utente ha scritto: " + kmInput.value);
-     console.log("L'utente ha scritto: " + ageInput.value);
-     if (ageInput.value=="senior") {
+btn.addEventListener("click", function() {
+    console.log("L'utente ha scritto: " + nameInput.value);
+    console.log("L'utente ha scritto: " + kmInput.value);
+    console.log("L'utente ha scritto: " + ageInput.value);
+    if (ageInput.value=="senior") {
         console.log("Sei vecchio");
     } else if (ageInput.value=="underage") {
         console.log("Sei minorenne");
@@ -16,5 +17,5 @@
         console.log("Sei adulto");
 
     }
- });
+});
 
