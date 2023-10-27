4d0f3d10dad24b2f98aa3d6b3256b549e1dd5e3a
DarioGuastella <jimmyguastella@gmail.com>
2023-10-27 17:35:35 +0200

    estrapola numero km

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const kmtariff = 0.21
+let kmInput  = Number( document.querySelector("#kilometers").value);
 let nameInput = document.querySelector("#username");
-let kmInput = document.querySelector("#kilometers");
 let ageInput = document.querySelector("#ageselect");
 let btn = document.querySelector("#generateBtn");
  
 
 btn.addEventListener("click", function() {
     console.log("L'utente ha scritto: " + nameInput.value);
-    console.log("L'utente ha scritto: " + kmInput.value);
+    console.log("L'utente ha scritto: " + kmInput);
     console.log("L'utente ha scritto: " + ageInput.value);
     if (ageInput.value=="senior") {
         console.log("Sei vecchio");
