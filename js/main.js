59e786621eff816127b394c62bc1991532ede893
DarioGuastella <jimmyguastella@gmail.com>
2023-10-27 16:25:59 +0200

    aggiunge console log

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
  let nameInput = document.querySelector("#username");
+ let kmInput = document.querySelector("#kilometers");
+ let ageInput = document.querySelector("#ageselect");
  let btn = document.querySelector("#generateBtn");
+ 
 
  btn.addEventListener("click", function() {
      console.log("L'utente ha scritto: " + nameInput.value);
+     console.log("L'utente ha scritto: " + kmInput.value);
+     console.log("L'utente ha scritto: " + ageInput.value);
+     if (ageInput.value=="senior") {
+        console.log("Sei vecchio");
+    } else if (ageInput.value=="underage") {
+        console.log("Sei minorenne");
+    } else {
+        console.log("Sei adulto");
+
+    }
  });
 
