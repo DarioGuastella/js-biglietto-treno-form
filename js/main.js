1c4cb3f664c3289023d48569cd270b80cc525c9c
DarioGuastella <jimmyguastella@gmail.com>
2023-10-27 18:32:03 +0200

    aggiunge testo e calcolo over 65 dopo il click

--- js/main.js
+++ js/main.js
@@ -9,9 +9,12 @@ btn.addEventListener("click", function() {
     console.log("L'utente ha scritto: " + nameInput.value);
     console.log("L'utente ha scritto: " + kmInput.value);
     console.log("L'utente ha scritto: " + ageInput.value);
-    
+    document.getElementById("nameId").innerHTML = nameInput.value;
     if (ageInput.value=="senior") {
-        console.log((Number(kmInput.value) * kmtariff) * 0.6);
+        document.getElementById("discountId").innerHTML = "Sconto Over 65";
+        document.getElementById("carriage").innerHTML = "5";
+        document.getElementById("codeCp").innerHTML = "92911";
+        document.getElementById("priceId").innerHTML = ((Number(kmInput.value) * kmtariff) * 0.6).toFixed(2) + " €";
     } else if (ageInput.value=="underage") {
         console.log((Number(kmInput.value) * kmtariff) * 0.8);
     } else {
