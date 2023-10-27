25afabd25016b4d11fb7dc5a8e37b5e14571937d
DarioGuastella <jimmyguastella@gmail.com>
2023-10-27 19:07:38 +0200

    riorganizza codice termina esercizio

--- js/main.js
+++ js/main.js
@@ -3,29 +3,29 @@ let kmInput  = document.querySelector("#kilometers");
 let nameInput = document.querySelector("#username");
 let ageInput = document.querySelector("#ageselect");
 let btn = document.querySelector("#generateBtn");
- 
-
+let discount = document.getElementById("discountId");
+let carriageId = document.getElementById("carriage");
+let priceId = document.getElementById("priceId");
+let codeId = document.getElementById("codeCp");
 btn.addEventListener("click", function() {
-    console.log("L'utente ha scritto: " + nameInput.value);
-    console.log("L'utente ha scritto: " + kmInput.value);
-    console.log("L'utente ha scritto: " + ageInput.value);
+    let carriage = parseInt((Math.random() * 9) + 1);
+    let cpCode = parseInt((Math.random() * 90000) + 1);
     document.getElementById("nameId").innerHTML = nameInput.value;
     if (ageInput.value=="senior") {
-        document.getElementById("discountId").innerHTML = "Sconto Over 65";
-        document.getElementById("carriage").innerHTML = "5";
-        document.getElementById("codeCp").innerHTML = "92911";
-        document.getElementById("priceId").innerHTML = ((Number(kmInput.value) * kmtariff) * 0.6).toFixed(2) + " €";
+        discount.innerHTML = "Sconto Over 65";
+        carriageId.innerHTML = carriage;
+        codeId.innerHTML = cpCode;
+        priceId.innerHTML = ((Number(kmInput.value) * kmtariff) * 0.6).toFixed(2) + " €";
     } else if (ageInput.value=="underage") {
-        document.getElementById("discountId").innerHTML = "Sconto Under 18";
-        document.getElementById("carriage").innerHTML = "5";
-        document.getElementById("codeCp").innerHTML = "92911";
-        document.getElementById("priceId").innerHTML = ((Number(kmInput.value) * kmtariff) * 0.8).toFixed(2) + " €";
+        discount.innerHTML = "Sconto Under 18";
+        carriageId.innerHTML = carriage;
+        codeId.innerHTML = cpCode;
+        priceId.innerHTML = ((Number(kmInput.value) * kmtariff) * 0.8).toFixed(2) + " €";
     } else {
-        document.getElementById("discountId").innerHTML = "Biglietto Standard";
-        document.getElementById("carriage").innerHTML = "5";
-        document.getElementById("codeCp").innerHTML = "92911";
-        document.getElementById("priceId").innerHTML = (Number(kmInput.value) * kmtariff).toFixed(2) + " €";
-        console.log(Number(kmInput.value) * kmtariff);
+        discount.innerHTML = "Biglietto Standard";
+        carriageId.innerHTML = carriage;
+        codeId.innerHTML = cpCode;
+        priceId.innerHTML = (Number(kmInput.value) * kmtariff).toFixed(2) + " €";
 
     }
 });
